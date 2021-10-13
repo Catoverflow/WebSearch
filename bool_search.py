de3a834246bdc4d2632166d3e7006968a8e85d84
Kanoe <marjorywalton1613@gmail.com>
2021-10-13 20:11:50 +0800

    modified object-oriented

--- bool_search.py
+++ bool_search.py
@@ -1,54 +1,66 @@
 # {A:{b,c,d}}，从倒排表建立的字典
-DIC={'the':{0,1,2,3,4,5},'a':{0,1,2,3,4,5},'one':{1},'two':{2},'three':{3}}
-reversedchar={"and","or","not","(",")"}
-#输入并初步处理，包括去小写，去多余空格，以空格和括号分词，将保留字之外的词直接转换为对应集合
-def inputprocess():
-    return [DIC[i] if i not in reversedchar else i for i in ' '.join(input("bool_search:input:search source:::")
-        .lower().replace('('," ( ").replace(')'," ) ").split()).split(' ')]
-#得到全集以处理not
-def getmost(inputlist):
-    most=set()
-    for i in inputlist:
-        if isinstance(i,set):
-            most=most.union(i)
-    return most
-#实际计算
-def calculate(list):
-    calist=list
-    most=getmost(list)
-    while len(calist)>1:
-        if "not" in calist:
-            for j in range(len(calist)):
-                if calist[j] == "not":
-                    calist[j+1]=most.difference(calist[j+1])
-                    del(calist[j])
-                    break
-        else:
-            for j in range(len(calist)):
-                if calist[j] == "and":
-                    calist[j+1]=calist[j-1].intersection(calist[j+1])
-                    del(calist[j-1:j+1])
-                    break
-                if calist[j] == "or":
-                    calist[j+1]=calist[j-1].union(calist[j+1])
-                    del(calist[j-1:j+1])
-                    break
-    return calist
+# DIC = {'the': {0, 1, 2, 3, 4, 5}, 'a': {0, 1, 2, 3, 4, 5}, 'one': {1}, 'two': {2}, 'three': {3}}
 
-#递归处理括号
-def bracketprocess(inputlist):
-    if(")" in inputlist):
-        r_index=inputlist.index(")")
-        inputlist_temp=inputlist[:r_index]
-        l_index=0
-        for i in range(len(inputlist_temp)):
-            if inputlist_temp[i] == "(":
-                l_index=i
-        inputlist_temp=inputlist[l_index+1:r_index]
-        inputlist[l_index:r_index+1]=calculate(inputlist_temp)
-        bracketprocess(inputlist)
-    return calculate(inputlist)
-#主函数。用来说明怎么让它跑起来。得到的结果是[{0,1,2}]这种形式。
-def main():
-    print(bracketprocess(inputprocess()))
-main()
+
+# userage: BoolSearch(docdic).processAndPrint()
+# docdic:example:{'the': {0, 1, 2, 3, 4, 5}, 'a': {0, 1, 2, 3, 4, 5}, 'one': {1}, 'two': {2}, 'three': {3}}
+class BoolSearch(object):
+    # init class with given inverted_index
+    def __init__(self, docdic):
+        self._docdic = docdic
+        self._reversedchar = {"and", "or", "not", "(", ")"}
+        self._inlist = []
+        self._most = set()
+
+    # input, preprocess and calcute the universial set
+    def _getInputAndMost_(self):
+        inputchr = input("BoolSearch***Input:\n")
+        self._inlist = [i if i in self._reversedchar else self._docdic[i] for i in
+                        inputchr.lower().replace('(', " ( ").replace(')', " ) ").split()]
+        for key in self._inlist:
+            if isinstance(key, set):
+                self._most.union(key)
+
+    # set calculate
+    def _calculate_(self, partlist):
+        calist = partlist
+        while len(calist) > 1:
+            if "not" in calist:
+                for j in range(len(calist)):
+                    if calist[j] == "not":
+                        calist[j + 1] = self._most.difference(calist[j + 1])
+                        del (calist[j])
+                        break
+            else:
+                for j in range(len(calist)):
+                    if calist[j] == "and":
+                        calist[j + 1] = calist[j - 1].intersection(calist[j + 1])
+                        del (calist[j - 1:j + 1])
+                        break
+                    if calist[j] == "or":
+                        calist[j + 1] = calist[j - 1].union(calist[j + 1])
+                        del (calist[j - 1:j + 1])
+                        break
+        return calist
+
+    # Recursively handle backets
+    def _backetProcess_(self):
+        if ")" in self._inlist:
+            r_index = self._inlist.index(")")
+            inputlist_temp = self._inlist[:r_index]
+            l_index = 0
+            for i in range(len(inputlist_temp)):
+                if inputlist_temp[i] == "(":
+                    l_index = i
+            inputlist_temp = self._inlist[l_index + 1:r_index]
+            self._inlist[l_index:r_index + 1] = self._calculate_(inputlist_temp)
+            self._backetProcess_()
+        return self._calculate_(self._inlist)
+
+    # External interface
+    def processAndPrint(self):
+        self._getInputAndMost_()
+        print(self._backetProcess_())
+
+# aBoolSearchTest = BoolSearch(DIC)
+# aBoolSearchTest.processAndPrint()
