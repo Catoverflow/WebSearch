ae381e84e830552b929dfda0e45b07c1d47ec664
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2021-10-21 22:13:47 +0800

    bug fixed

--- utils/inverted_index.py
+++ utils/inverted_index.py
@@ -7,14 +7,18 @@ class Inverted_Index(object):
     def __init__(self, data, dictionary):
         self.data = data
         self.dict = dictionary
-        self.inverted_index = {word:[] for word in self.dict}
+        self.inverted_index = {wordid:[] for wordid in range(len(self.dict))}
         self.word_count = []
+        # lookup table for word and id
+        self.getid = {self.dict[wordid]:wordid for wordid in range(len(self.dict))}
 
     def procecss(self):
         logging.info("Start generating inverted-index")
         for docid in range(len(self.data)):
             for word in self.data[docid]:
-                self.inverted_index[word].append(docid)
+                wordid = self.getid[word]
+                if docid not in self.inverted_index[wordid]:
+                    self.inverted_index[wordid].append(docid)
             if docid % 1000 == 0:
                 logging.debug(f'{docid} file processed')
-        self.word_count = [len(self.inverted_index[word]) for word in self.dict]
+        self.word_count = [len(self.inverted_index[wordid]) for wordid in range(len(self.dict))]
